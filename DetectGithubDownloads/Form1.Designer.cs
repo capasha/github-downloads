bd5d03a47c33a56990afbf0156eb8553edfd7b3d
capasha <capashaa@gmail.com>
2017-08-01 21:53:38 +0200

    Added the tool

---
+++ DetectGithubDownloads/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace DetectGithubDownloads
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(209, 198);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Version";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Downloads";
+            this.columnHeader2.Width = 100;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 284);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Status: Waiting";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 261);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Total Downloads: 0";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(227, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(221, 198);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 306);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Github Downloads";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ DetectGithubDownloads/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+namespace DetectGithubDownloads
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Dictionary<string, string> changelog = new Dictionary<string, string>();
+        private string version = "3.5.0";
+        private int totalDownloads = 0;
+        private string githubLink = "https://api.github.com/repos/capasha/eeditor/releases";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Status: Wating";
+            label2.Text = "Total Downloads: 0";
+            listView1.Items.Clear();
+            changelog.Clear();
+            richTextBox1.Clear();
+            string text;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(githubLink);
+                request.Method = "GET";
+                request.Accept = "application/vnd.github.v3+json";
+                request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; rv:48.0) Gecko/20100101 Firefox/48.0";
+   				using (var response = request.GetResponse() as HttpWebResponse)
+                    {
+                        if (request.HaveResponse && response != null)
+                        {
+                            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                            {
+                                text = reader.ReadToEnd();
+                            }
+                			dynamic stuff1 = Newtonsoft.Json.JsonConvert.DeserializeObject(text);
+                			foreach (var value in stuff1)
+                			{
+                    			if (value["tag_name"] != null) version = value["tag_name"];
+                                if (value["body"] != null) changelog.Add(version, value["body"].ToString());
+                            
+                                if (value["assets"] != null)
+                    			{
+                        			foreach (var val in value["assets"])
+                        			{
+                                    Console.WriteLine(val);
+                            			if (val["download_count"] != null)
+                            			{
+                                			listView1.Items.Add(version).SubItems.Add(val["download_count"].ToString());
+                                        totalDownloads += Convert.ToInt32(val["download_count"]);
+
+                                        }
+                        			}
+                        		}
+                        	}
+                        	label1.Text = "Status: Got the informations";
+                            label2.Text = "Total Downloads: " + totalDownloads;
+                        }
+                        else 
+                        {
+                        	label1.Text = "Status: Couldn't get the information";
+                        }
+                    }
+            }
+            catch (WebException ee)
+            {
+                if (ee.Status == WebExceptionStatus.Timeout)
+                {
+                    label1.Text = "Status: Took too long time to load the site";
+                }
+                else if (ee.Status == WebExceptionStatus.ProtocolError)
+                {
+                    label1.Text = "Status: Protocol Error";
+                }
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedIndices.Count > 0)
+            {
+                richTextBox1.Clear();
+                richTextBox1.Text = changelog[listView1.SelectedItems[0].Text];
+            }
+        }
+    }
+}
